508543544dcdb98e4476310070db8d0d0906545e
Deni Kusuma Wijaya <112800492+denikusumawijaya@users.noreply.github.com>
2024-07-08 23:49:41 +0700

    Create __init.py

---
+++ feedback-api/app/__init.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+Base = declarative_base()
